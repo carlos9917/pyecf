a745c3f5d58a0fd01808f55d21f772a8e9bec95b
Carlos Peralta <carlos9917@gmail.com>
2021-03-29 11:08:59 +0000

    Seems to work

--- call_ecfs.py
+++ call_ecfs.py
@@ -15,25 +15,19 @@ import numpy as np
 import fetch_utils as fu
 import date_utils as du
 
-epath = 'ec:/rml/precise/obs/obsoul/locobs/v11jun20/'
 
-def obs_path(obs,year,month):
-    obs_dirs = {'CONV':'LocalObsdata' ,
-                'OSISAF':os.path.join('OSISAF_data',str(year)) }
-    return obs_dirs[obs]
-
-def fetch_ecfs(obs,year,month,scratch,destination):
+def fetch_ecfs(obs,ecfspath,year,month,destination):
     '''
     single call to ecfs according to DTG
     '''
-    print("Checking observations %s"%obs)
     if obs == 'CONV':
-        fu.fetch_CONV(obs,year,month,scratch,destination)
+        fu.fetch_CONV(ecfspath,year,month,destination)
     elif obs == 'RO':
-        fu.fetch_RO(obs,year,month,scratch,destination)
+        tarball = "_".join(["GPSRO",str(year)+str(month).zfill(2),"Precise"])+".tar"
+        fu.fetch_RO(ecfspath,tarball,destination)
     else:
-        print(f"No implementation for {obs} just yet!")
-        print("Currently doing ONLY conventional observations (obsoul)")
+        print(f"No implementation for {ecfspath} just yet!")
+        print("Currently doing ONLY CONV and RO observations")
         sys.exit()
     return None
 
@@ -53,33 +47,42 @@ def read_yaml(file_path):
         return yaml.safe_load(f)
 
 def fetch_data(yaml_args):
-    obs = yaml_args["OBS"]
     year = yaml_args["CL_ARGS"]["YEAR"]
     month = yaml_args["CL_ARGS"]["MONTH"]
-    files_present = fu.check_if_present(obs,year,month)
+    obs = yaml_args["CL_ARGS"]["OBS"]
+    fpre = yaml_args["OBS"][obs]["FPRE"]
+    scratch =yaml_args["SCRATCH"]
+    obsdir = yaml_args["OBS"][obs]["OBSDIR"]
+    ecfspath = yaml_args["OBS"][obs]["ECFSPATH"]
+    #check first if data already there
+    destination = fu.create_destination(obsdir,year,month,scratch)
+    files_present = fu.test_if_present(obs,year,month,fpre,destination)
     if not files_present:
-        fu.create_destination(obsdir,year,month,scratch)
-        fetch_ecfs(args.obs,args.year,args.month,scratch,destination)
+        fetch_ecfs(obs,ecfspath,year,month,destination)
     else:
         print(f"Observations for {args.year}/{args.month} already copied!")
 
 def main(args):
+    HHOME = "/ws/home/ms/dk"
     #Read arguments from the yaml file
     yaml_args = read_yaml(args.yfile)
-    #Include the CL arguments
+
+    #Include the command line arguments
     yaml_args["CL_ARGS"] = {}
     yaml_args["CL_ARGS"]["OBS"] = args.obs
-    scratch = yaml_args["HARMONIE"]["SCRATCH"]
-    #destination = os.path.join(scratch,yaml_args["OBSFILES"][args.obs]["SCRPATH"])
-    # If user is not nhe, copy all to local tmp dir
-    cmd = "echo $USER"
+
+
+    # Check user's scratch directory and add it to yml options
+    cmd = "echo $SCRATCH"
     ret = subprocess.check_output(cmd,shell=True)
-    USER = ret.rstrip().decode('utf-8')
-    obsdir = yaml_args["OBSFILES"][args.obs]
-    if USER != "nhe":
-        scratch = scratch.replace("nhe",USER+"/tmp")
-    import pdb
-    pdb.set_trace()
+    SCRATCH = ret.rstrip().decode('utf-8')
+    #Change final dest to tmp location if only testing
+    if args.test:
+        print("This is a test!")
+        SCRATCH = os.path.join(SCRATCH,"tmp")
+    yaml_args["SCRATCH"] = SCRATCH
+    
+    #obsdir = yaml_args["OBS"][args.obs]
 
     #if year and month given, fetch data for that year and month
     # Otherwise go through all streams
@@ -93,10 +96,13 @@ def main(args):
             yaml_args["CL_ARGS"]["MONTH"] = args.month
             fetch_data(yaml_args)
     else:
-        streams = [st for st in data["STREAMS"].keys()]
+        #streams = [st for st in yaml_args["STREAMS"].keys()]
+        streams = [st for st in yaml_args["STREAMS"].keys() if yaml_args["STREAMS"][st]["ACTIVE"]]
+        print(f"Active streams: {streams}")
         yyyymm = []
         for st in streams:
-            hh = os.path.join(yaml_args["HARMONIE"]["HHOME"],yaml_args["STREAMS"][st]["USER"],"hm_home")
+            #The hhome path is only needed to read the progress.log file
+            hh = os.path.join(HHOME,yaml_args["STREAMS"][st]["USER"],"hm_home")
             DTG = du.get_current_dtg(hh,st)
             year,month = du.calc_year_month(DTG,mdays=15)
             print(f"Latest year and month from {st} progressMCI.log: {year} {month}")
@@ -104,14 +110,19 @@ def main(args):
                 yyyymm.append(year+"_"+month)
             else:
                 print(f"Doing nothing for {st}")
-        #remove all repeated
+        #remove all repeated values, so I dont request a month year more than once
         yyyymm = list(set(yyyymm))
         for ym in yyyymm:
             args.year = int(ym.split("_")[0])
             args.month = int(ym.split("_")[1])
             if args.year != None and args.month != None:
                 print(f"From progressMCI.log: need to fetch {args.year} {args.month}")
-                fetch_data(args,yaml_args)
+                yaml_args["CL_ARGS"]["YEAR"] = args.year
+                yaml_args["CL_ARGS"]["MONTH"] = args.month
+                fetch_data(yaml_args)
+            else:
+               print("Something went wrong reading the year or month from progressMCI.log")
+               print(f"year: {args.year}, month: {args.month}")
 
 if __name__=='__main__':
     import argparse
@@ -135,6 +146,7 @@ if __name__=='__main__':
                         required=False)
     #if this one is used it will get the year and month from the latest progress.log
     parser.add_argument('-auto',action='store_true') # set to false by default
+    parser.add_argument('-test',action='store_true') # set to false by default
     parser.add_argument('-yfile',metavar='name of the yaml config file',
                         type=str,
                         default="./streams.yaml",
--- fetch_utils.py
+++ fetch_utils.py
@@ -5,14 +5,15 @@ import numpy as np
 from collections import OrderedDict
 import re
 import date_utils as du
+import sys
 
-def fetch_CONV(obsdir,year,month,destination):
+def fetch_CONV(ecfspath,year,month,destination):
     '''
     This copies all conventional observations for a given month and year
     '''
-    lobs_epath = 'ec:/rml/precise/obs/obsoul/locobs/v11jun20/'
-    #First check if directory exists
-    obspath = os.path.join(lobs_epath,str(year),str(month).zfill(2))
+    print("Attemtping to fetch CONV observations")
+    obspath = os.path.join(ecfspath,str(year),str(month).zfill(2))
+    #First check if file(s)there
     try:
         cmd = "els "+obspath
         ret = subprocess.check_output(cmd,shell=True) 
@@ -25,13 +26,51 @@ def fetch_CONV(obsdir,year,month,destination):
         print("Error in subprocess {}".format(err))
         print("Directory {}")
        
-        
-    cmd="ecp "+os.path.join(lobs_epath,str(year),str(month).zfill(2),"*")+' '+destination
-    print("DEBUG obsoul command: %s"%cmd)
+    if len(files_found) != 0:
+        cmd="ecp "+os.path.join(obspath,"*")+' '+destination
+        print("DEBUG obsoul command: %s"%cmd)
+        try:
+            ret=subprocess.check_output(cmd,shell=True)
+        except subprocess.CalledProcessError as err:
+            print('obsoul monthly file not found %s'%err)
+    else:
+        print(f"No data found for {year} and {month}")
+
+def fetch_RO(ecfspath,tarball,destination):
+    '''
+    Fetch RO data. In this case we grab .tar balls
+    and uncompress them in destination directory
+    '''
+    print("Attemtping to fetch RO observations")
+    obspath = os.path.join(ecfspath,tarball)
     try:
-        ret=subprocess.check_output(cmd,shell=True)
+        cmd = "els "+obspath
+        ret = subprocess.check_output(cmd,shell=True) 
+        ret_clean = ret.rstrip().decode('utf-8')
+        files_found = ret_clean.split("\n") 
+        #add whole path to the file names:
+        files_found = [os.path.join(ecfspath,f) for f in files_found]
+        #print(f"Return from els: {files_found}")
     except subprocess.CalledProcessError as err:
-        print('obsoul monthly file not found %s'%err)
+        print("Error in subprocess {}".format(err))
+        print("Directory {}")
+       
+    if len(files_found) != 0:
+        cmd="ecp "+obspath+' '+destination
+        print("DEBUG RO command: %s"%cmd)
+        try:
+            ret=subprocess.check_output(cmd,shell=True)
+        except subprocess.CalledProcessError as err:
+            print('RO monthly file not found %s'%err)
+        #untar and delete file
+        cdir=os.getcwd()
+        os.chdir(destination)
+        cmd = "tar xvf "+tarball
+        ret=subprocess.check_output(cmd,shell=True)
+        os.remove(tarball)
+        os.chdir(cdir)
+    else:
+        print(f"No data found for {year} and {month}")
 
 def fetch_OSISAF(obsdir,year,month,destination):
     obsfile='carra_sst_iceconc_'
@@ -61,17 +100,22 @@ def fetch_OSISAF(obsdir,year,month,destination):
         print(f'Error running {cmd}: {err}')
 
 def create_destination(obsdir,year,month,scratch):
-    if obs == "CONV":
-        obspath=os.path.join(scratch,obsdir,year,month) #"LocalObsdata")
-    elif obs == "RO":
+    '''
+    Create destination directory if it doesnt exist
+    Return the path
+    '''
+    if obsdir == "LocalObsdata":
+        obspath=os.path.join(scratch,obsdir) #,year,month) #"LocalObsdata")
+    elif obsdir == "ROdata":
         obspath=os.path.join(scratch,obsdir)
 
     if not os.path.isdir(obspath):
         os.makedirs(obspath)
+    return obspath
 
 
 
-def check_if_present(obs,year,month,fpre): #scratch):
+def test_if_present(obs,year,month,fpre,destination): 
     '''
     Check if data already present. Currently assuming it is only locobs
     Not sure how many files are supposed to be available, but 
@@ -82,7 +126,7 @@ def check_if_present(obs,year,month,fpre): #scratch):
         fmin = 200 #threshold for obs. Completely arbitrary!
         obsYYMM = os.path.join(destination,fpre+"_"+str(year)+str(month).zfill(2))
     elif obs == "RO":
-        fmin = du.days_month(month,year)
+        fmin = 8 # Every 3 hours. du.days_month(month,year)
         obsYYMM = os.path.join(destination,fpre+"_"+str(year)+str(month).zfill(2))
     files_present=False
     import glob
