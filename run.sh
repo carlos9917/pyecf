afb0530a1d4b224e3660166efa0b63a8f6b3b4ea
Carlos Peralta <carlos9917@gmail.com>
2021-03-30 09:59:46 +0000

    Added bash script

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+module load python3
+
+#Usage
+#
+# Type python3 ./call_ecfs.py for full options
+
+# Examples:
+# 
+# python3 ./call_ecfs.py -auto -obs OBSTYPE
+# This will copy the observation type OBSTYPE if latest month and year in stream's MCI log is less than 15 days
+
+# python3 ./call_ecfs.py -month 1 -year 1998 -obs OBSTYPE
+# This will copy the observation type OBSTYPE for given year and month
+# 
+# Add -test option to test in local $SCRATCH/tmp directory (useful if testing in user other than running streams)
+
+
+#python3 ./call_ecfs.py -auto -obs CONV -test
+#python3 ./call_ecfs.py -auto -obs RO -test
+#python3 ./call_ecfs.py -month 8 -year 2020 -obs RO 
+
+python3 ./call_ecfs.py -auto -obs CONV -test
+python3 ./call_ecfs.py -auto -obs RO -test
