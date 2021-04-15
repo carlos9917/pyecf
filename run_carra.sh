1a5c27483dea1aed03ed39a2b1a6169831c4a938
Carra Project <nhx@cca-login4.ecmwf.int>
2021-04-15 14:47:02 +0000

    Added script and streams for CARRA

---
+++ run_carra.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+module load python3
+
+#Usage
+#
+# Type python3 ./call_ecfs.py for full options
+
+# Examples:
+# 
+# python3 ./call_ecfs.py -auto -obs OBSTYPE
+# This will copy the observation type OBSTYPE if latest month and year in stream's MCI log is less than 15 days
+
+# python3 ./call_ecfs.py -month 1 -year 1998 -obs OBSTYPE
+# This will copy the observation type OBSTYPE for given year and month
+# 
+# Add -test option to test in local $SCRATCH/tmp directory (useful if testing in user other than running streams)
+
+
+#python3 ./call_ecfs.py -auto -obs CONV -test
+#python3 ./call_ecfs.py -auto -obs RO -test
+#python3 ./call_ecfs.py -month 8 -year 2020 -obs RO 
+
+#python3 ./call_ecfs.py -auto -obs CONV -test
+#python3 ./call_ecfs.py -auto -obs RO -test -mdays 25
+#python3 ./call_ecfs.py -auto -obs CRYO -test -mdays 25
+MDAYS=30
+
+python3 ./call_ecfs.py -auto -obs CONV -yfile streams_carra.yaml -test -mdays $MDAYS
+python3 ./call_ecfs.py -auto -obs CRYO -yfile streams_carra.yaml -test -mdays $MDAYS
+python3 ./call_ecfs.py -auto -obs RO -yfile streams_carra.yaml -test -mdays $MDAYS
+python3 ./call_ecfs.py -auto -obs OSISAF -yfile streams_carra.yaml -test -mdays $MDAYS
+#python3 ./call_ecfs.py -auto -obs CRYO -yfile streams_carra.yaml -test -mdays 20
+#python3 ./call_ecfs.py -auto -obs RO -yfile streams_carra.yaml -test
